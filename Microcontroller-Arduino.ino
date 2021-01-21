bad5ff3a06df78c959620400699340329d33ac93
Gilles Bonné <bonne.gilles@gmail.com>
2021-01-21 12:31:11 +0100

    Implemented patterns via json.

--- Microcontroller-Arduino.ino
+++ Microcontroller-Arduino.ino
@@ -1,23 +1,63 @@
-String command;
+#include <ArduinoJson.h>
 
 void setup()
 {
-  Serial.begin(19200);
-  Serial.flush();
+    Serial.begin(9600);
+    Serial.flush();
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
-  {
-    command = Serial.readString();
-    Serial.println(command);
+    if (Serial.available())
+    {
+        // Allocate the JsonDocument.
+        StaticJsonDocument<1024> doc;
 
-    int indPin = command.indexOf(':');
-    int pin = command.substring(0,indPin).toInt();
-    int speed = command.substring(indPin+1).toInt();
-    Serial.println(pin);
-    Serial.println(speed);
-    analogWrite(pin, speed);
-  }
+        // Parse the JSON input via serial.
+        DeserializationError error = deserializeJson(doc, Serial);
+
+        // Parse succeeded?
+        if (error)
+        {
+            Serial.print("deserializeJson() returned ");
+            Serial.println(error.c_str());
+            return;
+        }
+        else
+        {
+            // Get a reference to the root array.
+            JsonArray patternArray = doc["pattern"].as<JsonArray>();
+            for (JsonObject vibration : patternArray)
+            {
+                // Get value from json document.
+                int duration = vibration["time"];
+
+                // Output prints.
+                Serial.print("vibrating for ");
+                Serial.print(duration);
+                Serial.print("ms on pins ");
+
+                // Get a reference to the root array.
+                JsonArray pinArray = vibration["pins"].as<JsonArray>();
+                for (JsonObject pinItem : pinArray)
+                {
+                    // Get values from json document.
+                    int pin = pinItem["pin"];
+                    int motorSpeed = pinItem["pwm"];
+
+                    // Set vibration motor speed.
+                    analogWrite(pin, motorSpeed);
+
+                    // Output prints.
+                    Serial.print(pin);
+                    Serial.print(":");
+                    Serial.print(motorSpeed);
+                    Serial.print(", ");
+                }
+                delay(duration);
+            }
+            // Output print. DO NOT REMOVE. Python will wait forever if there is no output.
+            Serial.println();
+        }
+    }
 }

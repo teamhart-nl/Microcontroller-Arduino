7ba599b38c6898ff48cb2c2b91c507072a7134f3
GillesBonne <bonne.gilles@gmail.com>
2021-02-28 12:38:56 +0100

    Change pca9685 controller library.

--- Microcontroller-Arduino.ino
+++ Microcontroller-Arduino.ino
@@ -1,25 +1,25 @@
 #include <ArduinoJson.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include "PCA9685.h"
 
-Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
+PCA9685 pwmController1(B000000);
+PCA9685 pwmController2(B000001);
 
 void setup()
 {
     // Start serial connection with a set baudrate.
     Serial.begin(115200);
 
-    pwm1.begin();
-    pwm2.begin();
+    Wire.begin();
 
-    pwm1.setOscillatorFrequency(27000000);
-    pwm2.setOscillatorFrequency(27000000);
+    pwmController1.resetDevices();
+    pwmController2.resetDevices();
 
-    pwm1.setPWMFreq(60);
-    pwm2.setPWMFreq(60);
+    pwmController1.init();
+    pwmController2.init();
 
-    Wire.setClock(400000);
+    pwmController1.setChannelOff(0);
+    pwmController2.setChannelOff(0);
 
     // Waits for transmission of outgoing serial data to complete.
     Serial.flush();
@@ -91,16 +91,16 @@ void SetVibration(const JsonObject &vibration)
     {
         int pin = pinItem["pin"];
         int motorSpeedPercentage = pinItem["pwm"];
-        int motorSpeed = (int) 4095 * motorSpeedPercentage / 100;
+        int motorSpeed = (int) 4095 * (motorSpeedPercentage / 100);
 
         // Set vibration motor speed.
         if (pin >= 0 && pin<16)
         {
-            pwm1.setPin(pin, motorSpeed, false);
+            pwmController1.setChannelPWM(pin, motorSpeed);
         }
         else if (pin >= 16 && pin<32)
         {
-            pwm2.setPin(pin, motorSpeed, false);
+            pwmController2.setChannelPWM(pin-16, motorSpeed);
         }
         else
         {
